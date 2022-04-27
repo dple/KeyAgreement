e9bc4380586fd6cf5af5e20c9324f28cb436d7f7
Phong Le <le.duc.phong@gmail.com>
2022-04-26 21:59:37 -0400

    Update load keys

--- src/ECDH.java
+++ src/ECDH.java
@@ -1,12 +1,16 @@
 import java.security.PublicKey;
 import java.security.PrivateKey;
 import java.security.KeyPairGenerator;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.spec.NamedParameterSpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.KeyAgreement;
 
 public class ECDH {
+	
 	/**
     * Convert bytes to Hex
     * @param bytes
@@ -17,16 +21,41 @@ public class ECDH {
 		for (byte temp : bytes) {
 			result.append(String.format("%02x", temp));
 		}
-		return result.toString();
+		return result.toString();		
 	}
 	
-	public static byte [] savePublicKey (PublicKey pub) throws Exception {		
+	public static byte[] savePublicKey (PublicKey pub) {		
 		return pub.getEncoded();
 	}
 
-	public static byte [] savePrivateKey (PrivateKey priv) throws Exception	{
+	public static PublicKey loadPublicKey (byte[] data) {
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("XDH");
+			X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(data);
+			return keyFactory.generatePublic(x509KeySpec);	
+		} catch (Exception e) {
+			System.out.println("loadPublicKey: Error has occured: " + e);
+			return null;
+		}
+		
+	}
+
+	public static byte [] savePrivateKey (PrivateKey priv){
 		return priv.getEncoded();		
 	}
+
+	public static PrivateKey loadPrivateKey (byte[] data){		
+		try {
+			KeyFactory keyFactory = KeyFactory.getInstance("XDH");
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(data);
+			return keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+		} catch (Exception e) {
+			System.out.println("loadPrivateKey: Error has occured: " + e);
+			return null;
+		}
+		
+	}
+
 
 	public static void doECDH (String name, PrivateKey priv, PublicKey pub) throws Exception	{
 		KeyAgreement ka = KeyAgreement.getInstance("XDH");		
@@ -49,16 +78,15 @@ public class ECDH {
 		PublicKey pubA = pairA.getPublic();
 		PrivateKey privB = pairB.getPrivate();
 		PublicKey pubB = pairB.getPublic();
+		byte[] dataPrvA = savePrivateKey(privA);
+		byte[] dataPubA = savePublicKey(pubA);
+		byte[] dataPrvB = savePrivateKey(privB);
+		byte[] dataPubB = savePublicKey(pubB);
 		
 		System.out.println("Alice: " + privA);
 		System.out.println("Alice: " + pubA);
 		System.out.println("Bob:   " + privB);
 		System.out.println("Bob:   " + pubB);
-		
-		byte [] dataPrvA = savePrivateKey(privA);
-		byte [] dataPubA = savePublicKey(pubA);
-		byte [] dataPrvB = savePrivateKey(privB);
-		byte [] dataPubB = savePublicKey(pubB);
 		
 		System.out.println("Alice Prv: " + bytesToHex(dataPrvA));
 		System.out.println("Alice Pub: " + bytesToHex(dataPubA));
